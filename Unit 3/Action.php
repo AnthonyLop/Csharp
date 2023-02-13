d2209f94803451fdaf1d424082c9c63eb1e34a38
AnthonyLop <lopezant@kean.edu>
2023-02-12 23:13:12 -0500

    first commit

---
+++ Unit 3/Action.php
@@ -0,0 +1,5 @@
+<?php
+echo "The form was received by: " . $_POST['recipient'] . "</br>";  echo "The subject is : " . $_POST['subject']. "</br>";
+echo "The name entered is : " . $_POST['name']. "</br>";
+echo "My comments are : " . $_POST['comments']. "</br>";  echo "My email is : " . $_POST['email']. "</br>";
+?>
